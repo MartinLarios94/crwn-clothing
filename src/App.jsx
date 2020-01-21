cba9c2913c96c1132a9752536b43d66983cc0b17
Martin Larios <Martin.Larios886@std.uni.edu.ni>
2020-01-20 21:02:30 -0600

    adding header component ad links to home, shop page and contacs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
+import './App.css';
 
 const HatsPage = () => (
   <div>
@@ -13,9 +15,10 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
